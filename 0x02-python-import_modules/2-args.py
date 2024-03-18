2c3fecf3348591460ddc2702cfd5c44632ad9648
root <root@behappy>
2024-03-18 21:20:39 +0000

    py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def dynamic(arr):
-    arrLength = len(arr)
-    if arrLength == 0:
-        print("0 arguments.")
-    elif arrLength == 1:
-        print("1 argument:\n1:", arr[0])
-    else:
-        print("{} arguments:".format(arrLength))
-        for (i, item) in enumerate(arr, start=1):
-            print(f"{i}: {item}")
-
-
-if (__name__ == '__main__'):
-    dynamic(argv[1:])
+if __name__ == "__main__":
+    from sys import argv
+    l = len(argv)
+    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
+                                 "." if l == 1 else ":"))
+    for i, s in enumerate(argv):
+        if i > 0:
+            print("{:d}: {:s}".format(i, s))
